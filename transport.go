4193eea8db38dd5f8c1aa5a5e3f98f441ec35bfc
Harry Bragg <harry.bragg@graze.com>
2019-03-21 13:32:22 +0000

    handle when existing transport is nil

--- transport.go
+++ transport.go
@@ -25,7 +25,11 @@ func NewTransport(base http.RoundTripper, limiter *rate.Limiter) *Transport {
 
 // WrapClient wraps an existing clients transport with the rate limiting transport
 func WrapClient(client *http.Client, limiter *rate.Limiter) *http.Client {
-	client.Transport = NewTransport(client.Transport, limiter)
+	ts := client.Transport
+	if ts == nil {
+		ts = http.DefaultTransport
+	}
+	client.Transport = NewTransport(ts, limiter)
 	return client
 }
 
